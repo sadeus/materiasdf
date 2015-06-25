e36fd9f2f18ac5ecb490c61e0032d6bc5b3874e7
sadeus <seba.schiavinato@gmail.com>
2015-06-25 01:10:52 -0300

    Wrapper working, with ising.c file.

--- ft3/ising/ising_wrapper.py
+++ ft3/ising/ising_wrapper.py
@@ -10,17 +10,19 @@ plt.rcParams["ytick.labelsize"] = 12
 plt.rcParams["xtick.labelsize"] = 12
 plt.rcParams["axes.labelsize"] = 20
 
-L = 50
+L = 64
 path = '.'
-temps = np.linspace(0.1,1,10)
+temps = np.linspace(0.1,1,20)
 fs = 100 #Cantidad de pasos entre mediciones.
-n_samp = 200 #Cantidad de sampleos
+n_samp = 2000 #Cantidad de sampleos
 warm = 500 #Cantidad de pasos antes de termalizar
+seed = 672791038.0
 sub.check_output(["gcc","-o","ising","ising.c","-lm"])
 out = "med_L_{}".format(L)
 filePath = os.path.join(path, out)
 mag = []
 e = []
+c = []
 for t in temps:
     cmd = ['./ising']
     cmd += ['-T', str(t)]
@@ -28,16 +30,17 @@ for t in temps:
     cmd += ['-n',str(n_samp)]
     cmd += ['-nT',str(warm)]
     cmd += ['-fs',str(fs)]
-    #cmd += ['-s', str(32232)]
-    out = "med_L_{}_{}".format(L,t)
+    cmd += ['-s', str(seed)]
+    out = "med_L_{}".format(L)
     #N = warm + fs * Nsamp #Cantidad de iteraciones finales
     filePath = os.path.join(path, out)
     open(filePath,'w+').close() #Lo crea de nuevo el archivo
     with open(filePath, "a+") as file:
         file.write(str(sub.check_output(cmd),"utf-8"))
     data = np.loadtxt(filePath)
-    mag.append(np.mean(np.abs(data[:,0])))
-    e.append(e)
+    mag.append(np.abs(np.mean(data[:,1])))
+    e.append(np.mean(data[:,2]))
+    c.append(np.var(data[:,2])**2)
 
 #Tamaño de figuras, siguiendo la regla de oro
 plt.figure(1)
@@ -45,24 +48,25 @@ plt.xlabel(r"$\beta J$")
 plt.ylabel(r'$\langle m \rangle$')           
 plt.plot(temps, mag, 'bo-')
 plt.grid()
-plt.axvline(np.log(1+ np.sqrt(2))/2, ls = "--", c = "k")
-#plt.savefig(os.path.join(path, 'mag_L_{}'.format(L)), bbox_inches = 'tight')
+#plt.axvline(np.log(1+ np.sqrt(2))/2, ls = "--", c = "k")
+plt.savefig(os.path.join(path, 'mag_L_{}'.format(L)), bbox_inches = 'tight')
 
 plt.figure(2)
 plt.xlabel(r"$\beta J$")
 plt.ylabel(r'$\langle e \rangle$')   
 plt.plot(temps, e,'ro-')
 plt.grid()
-plt.axvline(np.log(1+ np.sqrt(2))/2, ls = "--", c = "k")
-#plt.savefig(os.path.join(path, 'e_L_{}'.format(L)), bbox_inches = 'tight')
+#plt.axvline(np.log(1+ np.sqrt(2))/2, ls = "--", c = "k")
+plt.savefig(os.path.join(path, 'e_L_{}'.format(L)), bbox_inches = 'tight')
 
 
-#plt.figure(3)
-#plt.xlabel(r"$T' = \frac{k}{J} T$")
-#plt.ylabel(r"$c = \frac{<\Delta E>^2}{N T'^2}$")  
-#plt.plot(data[:,0], data[:,4]**2/(data[:,0])**2,'go-') 
-#plt.savefig(os.path.join(path, 'c_L_{}'.format(L)) , bbox_inches = 'tight')
+plt.figure(3)
+plt.xlabel(r"$T' = \frac{k}{J} T$")
+plt.ylabel(r"$c = \frac{<\Delta E>^2}{N T'^2}$")  
+plt.plot(temps, c,'go-') 
+plt.grid()
+plt.savefig(os.path.join(path, 'c_L_{}'.format(L)) , bbox_inches = 'tight')
 
-plt.show()
+#plt.show()
 
 #
